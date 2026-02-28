e06c8268edb13a93a4f11efea5357eb295b6f65a
Ares <lockyyw@outlook.com>
2026-02-28 19:51:44 +1100

    defering global.js and optimizing portfolio picture transition

--- src/script/global.js
+++ src/script/global.js
@@ -1,3 +1,9 @@
 import { initScrollReveal } from "@/components/helpers/scrollReveal";
 
-initScrollReveal();
+if (typeof window !== "undefined") {
+  if ("requestIdleCallback" in window) {
+    requestIdleCallback(initScrollReveal, { timeout: 1000 });
+  } else {
+    window.addEventListener("load", initScrollReveal);
+  }
+}
